68c469265b7f02f83100221d62f194f656c8b45b
Bhaskar Maddala <bhaskar@shutterstock.com>
2016-12-31 11:02:46 -0500

    complete chp17

--- chp17/src/chp17/core.clj
+++ chp17/src/chp17/core.clj
@@ -41,6 +41,8 @@
 
 (def MetaAnything {
   :__own_symbol__ 'MetaAnything
+  :__superclass_symbol__ 'Anything
+  :__class_symbol__ 'Anything
   :__instance_methods__ {
     :new (fn [type & args]
       (let [seeded {:__class_symbol__ (:__own_symbol__ type)}]
@@ -54,7 +56,8 @@
 
     :__instance_methods__ {
         :class (fn [this] (class-from-instance this))
-        :class-name :__class_symbol__ 
+        :class-name :__class_symbol__
+        :to-string (fn [this] (str this))
 
         :add-instance-values identity
 
@@ -68,6 +71,7 @@
 (def MetaPoint {
   :__own_symbol__ 'MetaPoint
   :__superclass_symbol__ 'MetaAnything
+  :__class_symbol__ 'Anything
   :__instance_methods__ {
     :origin (fn [type] (send-to type :new 0 0))
   }
@@ -80,12 +84,42 @@
     :__superclass_symbol__ 'Anything
     :__instance_methods__ {
         :add-instance-values (fn [this x y]
-            (assoc this :x x :y y)
-        )
+            (assoc this :x x :y y))
+
+        :x :x
+        :y :y
 
         :shift (fn [this xinc yinc]
             (send-to Point :new (+ (:x this) xinc)
-                        (+ (:y this) yinc))
+                        (+ (:y this) yinc)))
+
+        :add (fn [this other]
+            (send-to this :shift (:x other) (:y other)))
+
+        :to-string (fn [this]
+            (cl-format nil "A ~A like this: [~A, ~A]"
+                (send-to this :class-name)
+                (send-to this :x)
+                (send-to this :y)))
+    }
+})
+
+(def MetaMissingOverrider {
+    :__own_symbol__ 'MetaMissingOverrider
+    :__superclass_symbol__ 'MetaAnything
+
+    :__instance_methods__ {}
+})
+
+(def MissingOverrider {
+    :__own_symbol__ 'MissingOverrider
+    :__class_symbol__ 'MetaMissingOverrider
+    :__superclass_symbol__ 'Anything
+
+    :__instance_methods__ {
+        :method-missing (fn [this message args]
+            (println (cl-format nil "method-missing called! ~A on ~A." message this))
+            (println (cl-format nil "The arguments were ~A." args))
         )
     }
 })
